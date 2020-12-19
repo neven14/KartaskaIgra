425ea2c0283d2077d0bfe359582efd356495bc34
Neven <58472164+neven14@users.noreply.github.com>
2020-12-19 15:27:12 +0100

    Add files via upload

---
+++ KartaskaIgra/KartaskaIgra/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace KartaskaIgra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          int karte, bodovi = 0;
+            while (bodovi < 31)
+            {
+              Console.Write("Upisite broj karte:");
+                karte = Convert.ToInt32(Console.ReadLine());
+                if (karte > 13 || karte < 1)
+                {
+                 Console.WriteLine("Krivi unos!");
+                 
+                }
+                bodovi += karte;
+            }
+       if (bodovi == 31)
+            {
+                Console.WriteLine("Pobjedili ste!");
+            }
+        else
+            {
+                Console.WriteLine("Izgubili ste!");
+            }
+        }
+    }
+}
